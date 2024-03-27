586b4636bb66690145f12473b7651bb6e91a7a7d
gabriel <gabriell_rsilva@outlook.com>
2024-03-27 16:36:56 -0300

    sobre List

---
+++ collections/Main2.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        // Sobre List
+        // Principais Implementações
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        Vector<Pessoa> vectorList = new Vector<>();
+    }
+
+    static class Pessoa {
+
+    }
+}
