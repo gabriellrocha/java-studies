16654ceb7402589f7d5ad0067a58332aab8c028c
gabriel <gabriell_rsilva@outlook.com>
2024-03-30 09:20:12 -0300

    update: sobre List

--- collections/Main2.java
+++ collections/Main2.java
@@ -1,8 +1,6 @@
 package collections;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Vector;
+import java.util.*;
 
 public class Main2 {
 
@@ -12,10 +10,21 @@ public class Main2 {
         // Principais Implementações
 
         ArrayList<String> arrayList = new ArrayList<>();
-        
-        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        // Possui métodos para trabalhar com o inicio e o fim da lista, o que no ArrayList não possui
+
+        LinkedList<Integer> linkedList = new LinkedList<>(Arrays.asList(3, 8, 1, 15, 12, 9, 17, 13));
+
+        linkedList.addFirst(6); // adiciona no inicio
+        linkedList.addLast(2); // adiciona no final
+
+        linkedList.peekFirst(); // retorna o primeiro
+        linkedList.peekLast(); // retorna o último
 
         Vector<Pessoa> vectorList = new Vector<>();
+
+
+
     }
 
     static class Pessoa {
